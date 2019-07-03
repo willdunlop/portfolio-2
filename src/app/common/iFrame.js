f79f783ccb9736aaa215e997755a019807c784e0
William Dunlop <william.dunlop.1993@gmail.com>
2019-07-03 23:28:36 +1000

    added in to the iframe component

--- src/app/common/iFrame.js
+++ src/app/common/iFrame.js
@@ -9,16 +9,21 @@ class IFrame {
 
     render() {
         const jsx = <div className="iframe-comp">
-            <a 
-                className="iframe-comp__site-link"
-                href={this.props.url}
-                target="_blank"
-                rel="noopener noreferrer"
-            >Go to site</a>
             <iframe
                 className="iframe-comp__iframe"
                 src={this.props.url}
             ></iframe>
+            <div className="iframe-comp__description">
+                <h1 className="iframe-comp__title">Herbs of Gold</h1>
+                <p>Herbs of Gold is a herbal and nutritional supplements vendor</p>
+                <a 
+                    className="iframe-comp__site-link"
+                    href={this.props.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >Go to site</a>
+            </div>
+
         </div>;
 
         return jsx;
